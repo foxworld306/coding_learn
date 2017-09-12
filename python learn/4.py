1cd66d41efddbcf683c14d3d48c6264654dd19f6
foxworld306 <rio.zhao@gmail.com>
2017-09-12 23:19:32 +0800

    9-12 night

--- python learn/4.py
+++ python learn/4.py
@@ -1,14 +1,47 @@
+# _*_ encoding:utf-8 _*_
+
+
 # # 字典用{},字典中的key是唯一的，value不唯一
 # phonebook = {'ab':'0123', 'zd':'1234', 'ev':'2345'}
 # print(phonebook['ab'])
 
-# dict函数
-s = dict(name='zd', age='40')
-print(s)
-print(len(s))
-print(s['name'])
-s['name'] = 'pc'
-print(s)
-p = {}
-p['country'] = 'china' #将china赋值给country
-print(p)
+# # dict函数
+# s = dict(name='zd', age='40')
+# print(s)
+# print(len(s))
+# print(s['name'])
+# s['name'] = 'pc'
+# print(s)
+# p = {}
+# p['country'] = 'china' #将china赋值给country
+# print(p)
+
+people  ={
+    'Alice': {
+        'Phone':'8484',
+        'Addr': 'gogo street 123'
+    },
+    'Beth':{
+        'Phone': '3848',
+        'Addr': 'comco 1324'
+    },
+    'Ceser':{
+        'Phone':'1234',
+        'Addr':'dosscom 23'
+    }
+}
+
+labels = {
+    'Phone':'phone number',
+    'Addr': 'Address'
+}
+name = input('your name is: ')
+request = input('Phone number (p) or address (a)? ')
+
+# Use the correct key:
+if request == 'p': key = 'Phone'
+if request == 'a': key = 'Addr'
+
+# Only try to print information if the name is a valid key in
+# our dictionary:
+if name in people: print("{}'s {} is {}.".format(name, labels[key], people[name][key]))
