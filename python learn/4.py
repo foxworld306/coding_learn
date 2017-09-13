78fb9d3db658e873d2243a72e41eddb991e259b5
foxworld306 <rio.zhao@gmail.com>
2017-09-13 18:13:12 +0800

    usercf uploaded

--- python learn/4.py
+++ python learn/4.py
@@ -16,32 +16,35 @@
 # p['country'] = 'china' #将china赋值给country
 # print(p)
 
-people  ={
-    'Alice': {
-        'Phone':'8484',
-        'Addr': 'gogo street 123'
-    },
-    'Beth':{
-        'Phone': '3848',
-        'Addr': 'comco 1324'
-    },
-    'Ceser':{
-        'Phone':'1234',
-        'Addr':'dosscom 23'
-    }
-}
-
-labels = {
-    'Phone':'phone number',
-    'Addr': 'Address'
-}
-name = input('your name is: ')
-request = input('Phone number (p) or address (a)? ')
-
-# Use the correct key:
-if request == 'p': key = 'Phone'
-if request == 'a': key = 'Addr'
-
-# Only try to print information if the name is a valid key in
-# our dictionary:
-if name in people: print("{}'s {} is {}.".format(name, labels[key], people[name][key]))
+# people  ={
+#     'Alice': {
+#         'Phone':'8484',
+#         'Addr': 'gogo street 123'
+#     },
+#     'Beth':{
+#         'Phone': '3848',
+#         'Addr': 'comco 1324'
+#     },
+#     'Ceser':{
+#         'Phone':'1234',
+#         'Addr':'dosscom 23'
+#     }
+# }
+#
+# labels = {
+#     'Phone':'phone number',
+#     'Addr': 'Address'
+# }
+# name = input('your name is: ')
+# request = input('Phone number (p) or address (a)? ')
+#
+# # Use the correct key:
+# if request == 'p': key = 'Phone'
+# if request == 'a': key = 'Addr'
+#
+# # Only try to print information if the name is a valid key in
+# # our dictionary:
+# if name in people: print("{}'s {} is {}.".format(name, labels[key], people[name][key]))
+scores=[("zhang",100),("wang",98),("li",78)]
+print(scores[0])
+print(scores[0][0])
