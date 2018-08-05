2be13c2003b61ad44067bd8b5707fee1c05ac8ab
foxworld306 <foxworld306@163.com>
2018-08-05 22:58:16 +0800

    1

---
+++ Python learning/2.py
@@ -0,0 +1,20 @@
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+# html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+# bs = BeautifulSoup(html, "html.parser")
+# # nameList = bs.findAll('span', {'class': 'green'})
+# # for name in nameList:
+# #     print(name.get_text())
+
+# # alltext = bs.findAll(id="text")
+# # print(alltext[0].get_text())
+# # print(bs.findAll(lambda tag: len(tag.attrs) == 2))
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html, 'lxml')
+
+for child in bs.find('table',{'id':'giftList'}).children:
+    print(child)
---
+++ Python learning/3.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# html = urlopen('https://www.baidu.com')
+# bs = BeautifulSoup(html, 'html.parser')
+# print(bs.findAll('body'))
+
